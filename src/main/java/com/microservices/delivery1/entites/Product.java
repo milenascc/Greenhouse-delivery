5526f46f3b142a012f5bd2df68bac3fffde98de1
Rielson-Leandro <rlsl@cin.ufpe.br>
2018-06-25 12:00:48 -0300

    Alterações em Product

--- src/main/java/com/microservices/delivery1/entites/Product.java
+++ src/main/java/com/microservices/delivery1/entites/Product.java
@@ -6,48 +6,90 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "Product")
+// @Table(name = "Product")
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotNull
-    @Length(min = 2, max = 30)
-    private String name;
+	@NotNull
+	@Length(min = 2, max = 30)
+	private String name;
 
-    @NotNull
-    private Double price;
+	@NotNull
+	private Double price;
 
-    public Product(){}
-    public Product(Long id , String name, Double price){
-        super();
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
+	public Product() {
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Product(Long id, String name, Double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Double getPrice() {
-        return price;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (obj == null) {
+			return false;
+		}
+
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public String toString(){
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
 }
