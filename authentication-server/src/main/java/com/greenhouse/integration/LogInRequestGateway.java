4c075793aea1fe5bb3d8a8d9235215ef1aab3597
Rielson-Leandro <rlsl@cin.ufpe.br>
2018-07-13 20:50:25 -0300

    Autenticação precisa de alguns ajustes

---
+++ authentication-server/src/main/java/com/greenhouse/integration/LogInRequestGateway.java
@@ -0,0 +1,15 @@
+package com.greenhouse.integration;
+
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+@MessagingGateway
+public interface LogInRequestGateway {
+
+    @Gateway(requestChannel = "authServerAccountServiceBridgeInboundChannel",
+            replyChannel = "authServerAccountServiceBridgeOutboundChannel",
+            replyTimeout = 60*1000)
+    UserDetails getPrincipleByUSerName(String userName);
+}
