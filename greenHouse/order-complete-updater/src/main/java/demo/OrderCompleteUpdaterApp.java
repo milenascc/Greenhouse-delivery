72ebf0e9a2918dbfc665d2dabf6268a95f358cfe
Ramon Saboya <ramonsaboya@fb.com>
2019-10-23 01:09:40 -0300

    remove eureka and make docker-compose OSX compatible

--- greenHouse/order-complete-updater/src/main/java/demo/OrderCompleteUpdaterApp.java
+++ greenHouse/order-complete-updater/src/main/java/demo/OrderCompleteUpdaterApp.java
@@ -3,10 +3,8 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableCircuitBreaker
 public class OrderCompleteUpdaterApp {
     public static void main(String[] args) {
